4387404e4b29d35863f615c8edc12a53b71673ca
JV-R18 <81484824+JV-R18@users.noreply.github.com>
2025-05-29 11:32:39 -0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    nome: String,
+    senha: String,
+    tipo: String,
+    disponibilidade: Object
+});
+
+module.exports = mongoose.model('User', UserSchema);
