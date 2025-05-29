25bc3a3f2679019451db794efa319cf5c1ea89f0
JV-R18 <81484824+JV-R18@users.noreply.github.com>
2025-05-29 11:35:09 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require('./routes/users');
 
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://rezendejose15:UNd0Y7QuYD6fpbLa@classflow.jnfsa1y.mongodb.net/?retryWrites=true&w=majority&appName=classflow', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
